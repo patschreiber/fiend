f63fd14facb07cfa2292e48130f2c194f1901ef5
Pat Schreiber <ptos382@gmail.com>
2019-06-11 00:29:18 -0500

    Tinkering with game loop deltas

--- src/engine/FiendGame.js
+++ src/engine/FiendGame.js
@@ -3,18 +3,42 @@
  * here.
  */
 class FiendGame {'use strict';
+
   constructor() {
     /**
      * The ID returned from our main loop's most recent call to
      * requestAnimationFrame(). The token can then be used when we call
      * cancelAnimationFrame() to stop the main loop by telling the browser to
      * cancel the request that corresponds to our token.
+     * @type {number}
      */
     this.stopToken = null;
+
+    /**
+     * How frequently the game state updates. It is 20 Hz (50ms)
+     * @type {number}
+     */
+    this.tickLength = 50;
+
+    /**
+     * The most recently elapsed tick of the game clock.
+     * @type {double} DOMHighResTimeStamp
+     */
+    this.lastTick = performance.now();
   }
 
+  update(delta) {
+    this.lastTick = this.lastTick + this.tickLength;
+    console.log('this.lastTick :', this.lastTick);
+    console.log('delta :', delta);
+  }
+
+  /**
+   * Stops the main game loop.
+   */
   stopMainLoop() {
     window.cancelAnimationFrame(this.stopToken);
     console.log("cancelled");
   }
+
 }
--- src/main.js
+++ src/main.js
@@ -8,10 +8,25 @@
 ;(function() {'use strict';
 
   let FG = new FiendGame();
+  let R = new Renderer();
 
   /**
    * The main game loop. We use requestAnimationFrame to be thread-safe and not
    * dominate the browser when the player blurs focus on our tab.
+   *
+   * render() is passed tFrame because it is assumed that the render method will
+   *          calculate how long it has been since the most recently passed
+   *          update tick for extrapolation (purely cosmetic for fast devices).
+   *          It draws the scene.
+   *
+   * update() calculates the game state as of a given point in time. It should
+   *          always increment by tickLength. It is the authority for game
+   *          state. It is passed the DOMHighResTimeStamp for the time it
+   *          represents (which, again, is always last update + FG.tickLength
+   *          unless a pause feature is added, etc.)
+   *
+   * init()   Performs whatever tasks are needed before the main loop must run.
+   *
    *
    * @param {DOMHighResTimeStamp} tFrame The number of milliseconds since
    * navigationStart (when the previous document is unloaded.
@@ -19,11 +34,16 @@
    * callbacks as an argument when they are executed.
    */
   function main(tFrame) {
-    // Store the ID returned from our main loop's most recent call to requestAnimationFrame()
+    // Store the ID returned from our main loop's most recent call to
+    // requestAnimationFrame().
     FG.stopToken = window.requestAnimationFrame(main);
 
-    FG.stopMainLoop();
+    // Delta should be in seconds, not ms, so we divide by 1000.
+    let delta = (tFrame - FG.lastTick) / 1000.0;
+
+    FG.update(delta);
+    R.render();
   }
 
-  main();
+  main(performance.now());
 })();
